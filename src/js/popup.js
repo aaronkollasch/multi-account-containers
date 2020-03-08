4b05276d82be44cba786cca90540bb112de106d7
Sherry Ma <sherry.hyma@gmail.com>
2020-03-07 23:07:02 -0500

    change method name

--- src/js/popup.js
+++ src/js/popup.js
@@ -1823,7 +1823,7 @@ Logic.registerPanel(P_CONTAINER_EDIT, {
     Logic.setOrRemoveAssignment(tabId, fullURL, userContextId, false);
   },
 
-  checkurl(url){
+  checkUrl(url){
     // append "https://" if protocol not found
     const regexWww = /.*www\..*/g;
     const foundWww = url.match(regexWww);
