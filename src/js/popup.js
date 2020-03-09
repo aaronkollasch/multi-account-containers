fbeb43812a8dd8ddff0255202b5a4cf7e4c6582c
Sherry Ma <sherry.hyma@gmail.com>
2020-03-09 17:18:14 -0400

    added test cases for manually assigning URL

--- src/js/popup.js
+++ src/js/popup.js
@@ -1806,7 +1806,7 @@ Logic.registerPanel(P_CONTAINER_EDIT, {
     const tabId = currentTab.id;
     const fullURL = this.checkUrl(url);
 
-    if (fullURL !== "") {
+    if (fullURL !== null) {
       // Assign URL to container
       await Logic.setOrRemoveAssignment(tabId, fullURL, userContextId, false);
 
@@ -1828,7 +1828,7 @@ Logic.registerPanel(P_CONTAINER_EDIT, {
     let newURL = url;
 
     if (!url.match(validUrl)) {
-      return "";
+      return null;
     }
 
     if (!url.match(regexhttp) && !url.match(regexhttps)) {
