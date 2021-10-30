385cda069c007ee05a4687339bec5760d4d35906
Aaron Kollasch <aaron@kollasch.dev>
2021-10-30 02:06:40 -0400

    #1427 - Added backup/restore buttons in the Options UI

--- src/js/options.js
+++ src/js/options.js
@@ -88,3 +88,33 @@ for (let i=0; i < NUMBER_OF_KEYBOARD_SHORTCUTS; i++) {
   document.querySelector("#open_container_"+i)
     .addEventListener("change", storeShortcutChoice);
 }
+
+window.addEventListener("load", () => {
+  const backupLink = document.getElementById("containers-save-link");
+  document.getElementById("containers-save-button").addEventListener("click", async () => {
+    const content = JSON.stringify(
+      await browser.runtime.sendMessage({
+        method: "backupIdentitiesState"
+      })
+    );
+    backupLink.href = `data:application/json;base64,${btoa(content)}`;
+    backupLink.download = `containers-backup-${(new Date()).toISOString()}.json`;
+    backupLink.click();
+  }, { capture: true, passive: false });
+
+  const restoreInput = document.getElementById("containers-restore-input");
+  restoreInput.addEventListener("change", () => {
+    if (restoreInput.files.length) {
+      const reader = new FileReader();
+      reader.onloadend = async () => {
+        const identitiesState = JSON.parse(reader.result);
+        await browser.runtime.sendMessage({
+          method: "restoreIdentitiesState",
+          identities: identitiesState
+        });
+      };
+      reader.readAsText(restoreInput.files.item(0));
+    }
+    restoreInput.reset();
+  });
+});
