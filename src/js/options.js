aeb9d96b0e5a9c1ddbdf28d71c7050ca0a2b5d90
Aaron Kollasch <aaron@kollasch.dev>
2021-10-30 02:06:40 -0400

    1427 - Added backup/restore buttons in the Options UI

--- src/js/options.js
+++ src/js/options.js
@@ -131,6 +131,36 @@ for (let i=0; i < NUMBER_OF_KEYBOARD_SHORTCUTS; i++) {
     .addEventListener("change", storeShortcutChoice);
 }
 
+window.addEventListener("load", () => {
+  const backupLink = document.getElementById("containers-save-link");
+  document.getElementById("containers-save-button").addEventListener("click", async () => {
+    const content = JSON.stringify(
+      await browser.runtime.sendMessage({
+        method: "backupIdentitiesState"
+      })
+    );
+    backupLink.href = `data:application/json;base64,${btoa(content)}`;
+    backupLink.download = `containers-backup-${(new Date()).toISOString()}.json`;
+    backupLink.click();
+  }, { capture: true, passive: false });
+
+  const restoreInput = document.getElementById("containers-restore-input");
+  restoreInput.addEventListener("change", () => {
+    if (restoreInput.files.length) {
+      const reader = new FileReader();
+      reader.onloadend = async () => {
+        const identitiesState = JSON.parse(reader.result);
+        await browser.runtime.sendMessage({
+          method: "restoreIdentitiesState",
+          identities: identitiesState
+        });
+      };
+      reader.readAsText(restoreInput.files.item(0));
+    }
+    restoreInput.reset();
+  });
+});
+
 document.querySelectorAll("[data-btn-id]").forEach(btn => {
   btn.addEventListener("click", () => {
     switch (btn.dataset.btnId) {
